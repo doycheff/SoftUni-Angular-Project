c39aa2e0019c41a6c4b443568161dfa22946bdc5
Plamen Doychev <125978729+doycheff@users.noreply.github.com>
2024-11-24 17:11:26 +0200

    Create login and create-book components

--- Angular-project/src/app/book/create-book/create-book.component.ts
+++ Angular-project/src/app/book/create-book/create-book.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../api.service';
 
 @Component({
-  selector: 'app-create-book',
-  standalone: true,
-  imports: [],
-  templateUrl: './create-book.component.html',
-  styleUrl: './create-book.component.css'
+    selector: 'app-create-book',
+    standalone: true,
+    imports: [],
+    templateUrl: './create-book.component.html',
+    styleUrl: './create-book.component.css'
 })
 export class CreateBookComponent {
+    constructor(private apiService: ApiService) { };
 
+    addBook(event: Event, title: string, genre: string, price: string, description: string, image: string) {
+        event.preventDefault();
+
+        this.apiService.createBook(title, genre, price, description, image).subscribe(data => {
+            console.log(data);
+        })
+    }
 }
