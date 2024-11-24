c2d8c7ee891fb08818b30461b4806857acec282a
Plamen Doychev <125978729+doycheff@users.noreply.github.com>
2024-11-24 15:23:08 +0200

    Create basic user service

---
+++ Angular-project/src/app/user/user.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { UserForAuth } from '../types/user';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class UserService {
+    USER_KEY = '[user]';
+    user: UserForAuth | null = null;
+
+    get isLogged(): boolean {
+        return !!this.user;
+    }
+
+    constructor() {
+        try {
+            const lsUser = localStorage.getItem(this.USER_KEY) || '';
+            this.user = JSON.parse(lsUser);
+        } catch (error) {
+            this.user = null;
+        }
+    }
+
+    login() {
+        this.user = {
+            email: 'john.doe@abv.bg',
+            password: '123123',
+            id: 'asdasdsadsadsa',
+        };
+
+        localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+    }
+
+    logout() {
+        this.user = null;
+        localStorage.removeItem(this.USER_KEY);
+    }
+}
