07506f4dc83fcf9c0de660ebc0a228a7dc22969c
Plamen Doychev <125978729+doycheff@users.noreply.github.com>
2024-12-01 17:19:56 +0200

    Update routes

--- Angular-project/src/app/app.routes.ts
+++ Angular-project/src/app/app.routes.ts
@@ -10,22 +10,34 @@ import { SearchComponent } from './search/search.component';
 import { AuthGuard } from './guards/auth.guard';
 import { EditBookComponent } from './book/edit-book/edit-book.component';
 import { LoggedInGuard } from './guards/logged-in.guard';
+import { ProfileComponent } from './user/profile/profile.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
 
-    { path: 'login', component: LoginComponent, canActivate: [LoggedInGuard]},
-    { path: 'register', component: RegisterComponent, canActivate: [LoggedInGuard]},
-    // { path: 'profile', component: ProfileComponent },
+  { path: 'login', component: LoginComponent, canActivate: [LoggedInGuard] },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    canActivate: [LoggedInGuard],
+  },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
 
-    { path: 'books', component: BooksListComponent },
-    { path: 'create-book', component: CreateBookComponent, canActivate: [AuthGuard] },
-    { path: 'books/:id', component: BookDetailsComponent},
-    { path: 'edit/:id', component: EditBookComponent, canActivate: [AuthGuard] },
-    { path: 'search', component: SearchComponent },
+  { path: 'books', component: BooksListComponent },
+  {
+    path: 'create-book',
+    component: CreateBookComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'books/:id', component: BookDetailsComponent },
+  {
+    path: 'edit/:id',
+    component: EditBookComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'search', component: SearchComponent },
 
-
-    { path: '404', component: ErrorComponent },
-    { path: '**', redirectTo: '/404', pathMatch: 'full' },
+  { path: '404', component: ErrorComponent },
+  { path: '**', redirectTo: '/404', pathMatch: 'full' },
 ];
